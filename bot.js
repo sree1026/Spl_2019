e1ad496fe21c2beb305b69e3beb293ac6720c6e6
Prithvi <prithvi.subaraja@solitontech.com>
2019-08-30 16:37:51 +0530

    Vertical, Horizontal Defense and Offense Moves
    Signed-off-by: Prithvi <prithvi.subaraja@solitontech.com>

--- bot.js
+++ bot.js
@@ -62,29 +62,66 @@ class Bot {
     result(data) {
         console.log("Game over ::", data.result);
     }
-    // checkHorizontal(boardInfo, data) {
-    //     let boardSize = data.boardSize;
-    //     let rows = boardSize[0];
-    //     let opponentId = data.yourID === 1 ? 2 : 1;
-    //     console.log("OPPONENT ID ========== ", opponentId);
-    //     let columns = boardSize[1];
-    //     let move = [];
-    //     let count = 0;
-    //     for (let i = 0; i < rows; i++) {
-    //         for (let j = 0; j < columns; j++) {
-    //             if (boardInfo[i][j] === opponentId && boardInfo[i][j + 1] === opponentId && boardInfo[i][j + 2] === opponentId) {
-    //                 if (j + 3 < columns && boardInfo[i][j + 3] === 0) {
-    //                     return [i][j + 3];
-    //                 } else {
-    //                     return 0;
-    //                 }
-    //             } else {
-    //                 return 0;
-    //             }
-    //         }
-    //     }
-    // }
 
+    horizontalDefensive(rows, columns, boardInfo, oppChargeID) {
+        for (let i = 0; i < rows; i++) {
+            let horizontalSequenceCount = 0;
+            for (let j = 0; j < columns; j++) {
+                if(boardInfo[i][j] === oppChargeID) {
+                    horizontalSequenceCount++;
+                }
+                else {
+                    horizontalSequenceCount = 0;
+                }
+                if(horizontalSequenceCount === 3) {
+                    console.log()
+                    if(boardInfo[i][j+1] === 0) {
+                        return [i, j+1];
+                    }
+                    else if(boardInfo[i][j-3] === 0) {
+                        return [i, j-3];
+                    }
+                }
+            }
+        }
+        return [];
+    }
+    
+    verticalDefensive(rows, columns, boardInfo, oppChargeID) {
+        for (let i = 0; i < columns; i++) {
+            let verticalSequenceCount = 0;
+            for (let j = 0; j < rows; j++) {
+                if(boardInfo[j][i] === oppChargeID) {
+                    verticalSequenceCount++;
+                }
+                else {
+                    verticalSequenceCount = 0;
+                }
+                if(verticalSequenceCount === 3) {
+                    if(boardInfo[j+1][i] === 0) {
+                        return [j+1, i];
+                    }
+                    else if(boardInfo[j-3][i] === 0) {
+                        return [j-3, i];
+                    }
+                }
+            }
+        }
+        return [];
+    }
+
+    defensiveMove(rows, columns, boardInfo, oppChargeID) {
+        let resultMove = this.horizontalDefensive(rows, columns, boardInfo, oppChargeID);
+        if( resultMove.length !== 0 ) {
+            return resultMove;
+        }
+        resultMove = this.verticalDefensive(rows, columns, boardInfo, oppChargeID);
+        if( resultMove.length !== 0 ) {
+            return resultMove;
+        }
+        return [];
+    }
+    
     horizontalOffensive(rows, columns, boardInfo, myChargeID) {
         for (let i = 0; i < rows; i++) {
             let horizontalSequenceCount = 0;
@@ -92,12 +129,39 @@ class Bot {
                 if(boardInfo[i][j] === myChargeID) {
                     horizontalSequenceCount++;
                 }
+                else {
+                    horizontalSequenceCount = 0;
+                }
                 if(horizontalSequenceCount === 3) {
+                    console.log()
                     if(boardInfo[i][j+1] === 0) {
                         return [i, j+1];
                     }
-                    else if(boardInfo[i][j-1] === 0) {
-                        return [i, j-1];
+                    else if(boardInfo[i][j-3] === 0) {
+                        return [i, j-3];
+                    }
+                }
+            }
+        }
+        return [];
+    }
+    
+    verticalOffensive(rows, columns, boardInfo, myChargeID) {
+        for (let i = 0; i < columns; i++) {
+            let verticalSequenceCount = 0;
+            for (let j = 0; j < rows; j++) {
+                if(boardInfo[j][i] === myChargeID) {
+                    verticalSequenceCount++;
+                }
+                else {
+                    verticalSequenceCount = 0;
+                }
+                if(verticalSequenceCount === 3) {
+                    if(boardInfo[j+1][i] === 0) {
+                        return [j+1, i];
+                    }
+                    else if(boardInfo[j-3][i] === 0) {
+                        return [j-3, i];
                     }
                 }
             }
@@ -106,7 +170,15 @@ class Bot {
     }
 
     offensiveMove(rows, columns, boardInfo, myChargeID) {
-        return this.horizontalOffensive(rows, columns, boardInfo, myChargeID);
+        let resultMove = this.horizontalOffensive(rows, columns, boardInfo, myChargeID);
+        if( resultMove.length !== 0 ) {
+            return resultMove;
+        }
+        resultMove = this.verticalOffensive(rows, columns, boardInfo, myChargeID);
+        if( resultMove.length !== 0 ) {
+            return resultMove;
+        }
+        return [];
     }
 
     linearOrder(rows, columns, boardInfo) {
@@ -132,22 +204,16 @@ class Bot {
         
         let rows = boardSize[0];
         let columns = boardSize[1];
-        let opponentChargeId = (myChargeID === 1)? 2 : 1;
-        
-        for (let i = 0; i < rows; i++) {
-            for (let j = 0; j < columns; j++) {
-                if (boardInfo[i][j] === opponentChargeId && boardInfo[i][j + 1] === opponentChargeId && boardInfo[i][j + 2] === opponentChargeId) {
-                    if (j + 3 < columns && boardInfo[i][j + 3] === 0) {
-                        return [i, j + 3];
-                    }
-                }
-            }
-        }
+        let oppChargeID = (myChargeID === 1)? 2 : 1;
 
-        let resultMove = this.offensiveMove(rows, columns, boardInfo, myChargeID);
+        let resultMove = this.defensiveMove(rows, columns, boardInfo, oppChargeID);
         if (resultMove.length !== 0) {
             return resultMove;
-        } 
+        }
+        resultMove = this.offensiveMove(rows, columns, boardInfo, myChargeID);
+        if (resultMove.length !== 0) {
+            return resultMove;
+        }
         return this.linearOrder(rows, columns, boardInfo);
     }
 
